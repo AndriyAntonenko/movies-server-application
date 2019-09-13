49660d7d0d4cdb5f4801c7556acd3907c6f3ac42
AndriyAntonenko <andriyantonenko3.16@gmail.com>
2019-09-13 13:19:13 +0300

    Fix bug with searching by stars

--- src/films/controllers/findByStar.js
+++ src/films/controllers/findByStar.js
@@ -3,28 +3,46 @@ const Film = require("../models/Film");
 module.exports = async function(req, res) {
   const { star } = req.query;
 
+  const data = {
+    _id: true,
+    title: true,
+    release: true,
+    format: true,
+    stars: true
+  };
   const films = await Film.aggregate([
     {
       $project: {
-        title: true,
-        release: true,
-        format: true,
-        stars: true,
+        ...data,
         starsInLowerCase: {
           $map: {
             input: "$stars",
             as: "star",
-            in: { $toLower: "$$star" }
+            in: { $split: ["$$star", " "] }
           }
         }
       }
     },
     {
-      $match: {
-        starsInLowerCase: { $elemMatch: { $eq: star.toLowerCase() } }
+      $unwind: {
+        path: "$starsInLowerCase"
       }
     },
-    { $project: { starsInLowerCase: false } }
+    {
+      $match: {
+        starsInLowerCase: {
+          $elemMatch: { $in: star.toLowerCase().split(" ") }
+        }
+      }
+    },
+    { $project: { starsInLowerCase: false } },
+    {
+      $group: {
+        _id: "$_id",
+        title: { $first: "$title" },
+        release: { $first: "$release" }
+      }
+    }
   ]);
 
   return res.status(200).json({ success: true, films });
